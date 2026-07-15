ae6bb6a94dbb9c314307af83abbc7d61ff110ffc
Matthew Miezaniec <matthewmiezaniec@yahoo.co.uk>
2026-07-15 18:37:43 +0100

    feat(datasets): add CSV/TSV manifest and YOLO loaders via a DATASET_LOADERS registry

--- src/phytovision/datasets/__init__.py
+++ src/phytovision/datasets/__init__.py
@@ -2,6 +2,8 @@ from phytovision.datasets.base import DatasetLoader, Sample
 from phytovision.datasets.coco import CocoDetectionLoader
 from phytovision.datasets.directory import ImageDirectoryLoader
 from phytovision.datasets.folder import FolderClassificationLoader
+from phytovision.datasets.manifest import CsvManifestLoader
+from phytovision.datasets.yolo import YoloDetectionLoader
 
 __all__ = [
     "DatasetLoader",
@@ -9,4 +11,6 @@ __all__ = [
     "FolderClassificationLoader",
     "ImageDirectoryLoader",
     "CocoDetectionLoader",
+    "CsvManifestLoader",
+    "YoloDetectionLoader",
 ]
